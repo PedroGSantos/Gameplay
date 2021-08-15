afc73083e274652f5aa3136433fcc990c0fd55ed
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2021-08-15 15:23:39 -0300

    finish lint configuration

--- App.tsx
+++ App.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-	const ed ="";
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
+			<StatusBar style='auto' />
 		</View>
 	);
 }
