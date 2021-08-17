be3aebaf5899609dd3b415a1eebba33faa2ff7de
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2021-08-17 12:23:53 -0300

    config fonts and svgs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style='auto' />
-		</View>
-	);
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular, Inter_500Medium, Rajdhani_500Medium, Rajdhani_700Bold
+	});
+	if (!fontsLoaded)
+		return <AppLoading />;
+	else 
+		return (
+			<View style={styles.container}>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style='auto' />
+			</View>
+		);
 }
 
 const styles = StyleSheet.create({
